f6dba228e83fd27b216458b7a6784631af98db5d
JeanMaxSKrebs <jeanmaxskrebs@gmail.com>
2023-04-05 15:50:32 -0300

    Init atividade05

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,60 @@
+import { initializeApp } from 'firebase/app'
+import {
+    getAuth
+} from 'firebase/auth';
+import {
+    getDatabase
+} from "firebase/database"
+// import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
+};
+
+class Firebase {
+
+    constructor() {
+        this.app = initializeApp(firebaseConfig)
+        this.db = getDatabase(this.app)
+        this.auth = getAuth();
+        this.isLogged = false;
+        this.credentials =null;
+    }
+
+    // *** AUTH API ***
+    async doCreateUserWithEmailAndPassword(email, password) {
+        try {
+           //implemente aqui a função para criar um usuário
+           throw new Error("Função indisponível") //remova essa linha
+        } catch (error) {
+            console.error(error.message)
+            throw error;
+        }
+    }
+
+    async doSignInWithEmailAndPassword(email, password) {
+        try {
+            /**
+            Quando o usuário estiver logado atribua o valor TRUE
+            ao atributo this.isLogged e as credenciais ao atributo this.credentials
+            deverá retornar a propriedade user do atributo this.credentials.user
+            */
+           //implemente aqui função logar o usuario
+           throw new Error("Função Indisponível!!") //remova essa linha
+        } catch (error) {
+            console.error(error.message)
+            throw error;
+        }
+    }
+
+    doSignOut = () => {
+    };
+}
+
+export default Firebase;
